559eedfe67f4723dc979911710d190e7d8b67aa0
ashwink0 <ashwin.kuppahally@gmail.com>
2021-10-02 10:08:29 -0700

    jfkfndndmdndn

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/go-redis/redis/v8"
 	"github.com/gorilla/websocket"
+	"github.com/nu7hatch/gouuid"
 	"log"
 	"net/http"
 )
@@ -10,6 +13,7 @@ var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 }
+var ctx = context.Background()
 
 func wsEndpoint(w http.ResponseWriter, r *http.Request) {
 	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
@@ -18,12 +22,18 @@ func wsEndpoint(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Println(err)
 	}
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	u, err := uuid.NewV4()
 	log.Println("Client Connected")
 
-	reader(ws)
+	reader(ws, rdb, u)
 }
 
-func reader(conn *websocket.Conn) {
+func reader(conn *websocket.Conn, rdb *redis.Client, uuid *uuid.UUID) {
 	for {
 		messageType, p, err := conn.ReadMessage()
 		if err != nil {
@@ -31,7 +41,10 @@ func reader(conn *websocket.Conn) {
 			return
 		}
 		fmt.Println(string(p))
-
+		errSet := rdb.Set(ctx, uuid.String(), string(p), 60).Err()
+		if errSet != nil {
+			panic(err)
+		}
 		if err := conn.WriteMessage(messageType, p); err != nil {
 			log.Println(err)
 			return
