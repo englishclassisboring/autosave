bfc0984e5d6d80df4cd807c1f0dcebc853aa9282
ashwink0 <ashwin.kuppahally@gmail.com>
2021-10-02 09:45:17 -0700

    dhsajkdhasj

--- main.go
+++ main.go
@@ -2,50 +2,50 @@ package main
 
 import (
 	"fmt"
-	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
+	"log"
 	"net/http"
 )
-
-func main() {
-
-	r := gin.Default()
-	r.LoadHTMLFiles("index.html")
-
-	r.GET("/", func(c *gin.Context) {
-		c.HTML(200, "index.html", nil)
-	})
-
-	r.GET("/ws", func(c *gin.Context) {
-		wshandler(c.Writer, c.Request)
-	})
-
-	r.Run("localhost:12312")
-}
-
-var wsupgrader = websocket.Upgrader{
+var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
 }
 
-func wshandler(w http.ResponseWriter, r *http.Request) {
-	conn, err := wsupgrader.Upgrade(w, r, nil)
-	fmt.Println("4732891047382190")
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
+	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		fmt.Println("Failed to set websocket upgrade: %+v", err)
-		return
+		log.Println(err)
 	}
+	log.Println("Client Connected")
 
+	reader(ws)
+}
+
+func reader(conn *websocket.Conn) {
 	for {
-		t, msg, err := conn.ReadMessage()
-
+		messageType, p, err := conn.ReadMessage()
 		if err != nil {
-			break
+			log.Println(err)
+			return
 		}
-		fmt.Println(msg)
-		conn.WriteMessage(t, msg)
+		fmt.Println(string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+
 	}
+}
+
+func setupRoutes() {
+	http.HandleFunc("/ws", wsEndpoint)
+}
+
+func main() {
+	fmt.Println("Online")
+	setupRoutes()
+	log.Fatal(http.ListenAndServe(":1234", nil))
 }
